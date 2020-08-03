415146620f119aa65173d523f4df52d6de9174b2
Christopher Lamm <chris@theantichris.com>
2020-08-03 09:14:08 -0400

    loading static assets

--- main.go
+++ main.go
@@ -7,32 +7,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday"
 )
 
-// ?: should I put this in env variables?
+// TODO: load from env
 const (
-	markdownFolder = "./markdown"
-
-	templateGlob = "./templates/*.tmpl.html"
-
-	indexTemplate = "index.tmpl.html"
-	postTemplate  = "post.tmpl.html"
-	errorTemplate = "error.tmpl.html"
+	assetsFolder   = "/assets"
+	markdownFolder = "/markdown"
+	templateGlob   = "/templates/*.tmpl.html"
+	indexTemplate  = "index.tmpl.html"
+	postTemplate   = "post.tmpl.html"
+	errorTemplate  = "error.tmpl.html"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(gin.Logger())
-
 	r.Delims("{{", "}}") // sets template tags
-	r.LoadHTMLGlob(templateGlob)
+
+	r.Use(static.Serve(assetsFolder, static.LocalFile("/assets", false)))
+	r.LoadHTMLGlob("." + templateGlob)
 
 	r.GET("/", func(c *gin.Context) {
 		var posts []string
 
-		files, err := ioutil.ReadDir(markdownFolder)
+		files, err := ioutil.ReadDir("." + markdownFolder)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -48,7 +49,7 @@ func main() {
 	r.GET("/:postTitle", func(c *gin.Context) {
 		postTitle := c.Param("postTitle")
 
-		mdFile, err := ioutil.ReadFile(markdownFolder + "/" + postTitle)
+		mdFile, err := ioutil.ReadFile("." + markdownFolder + "/" + postTitle)
 		if err != nil {
 			fmt.Println(err)
 			c.HTML(http.StatusNotFound, errorTemplate, nil)
@@ -58,7 +59,10 @@ func main() {
 
 		postContent := template.HTML(blackfriday.MarkdownCommon([]byte(mdFile)))
 
-		c.HTML(http.StatusOK, postTemplate, gin.H{"Title": postTitle, "Content": postContent})
+		c.HTML(http.StatusOK, postTemplate, gin.H{
+			"Title":   postTitle,
+			"Content": postContent,
+		})
 	})
 
 	r.Run(":8080")
