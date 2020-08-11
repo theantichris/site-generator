381665dfcd8a0af09d77709f82aa4cb7a599c209
Christopher Lamm <chris@theantichris.com>
2020-08-11 08:30:26 -0400

    created options struct

--- main.go
+++ main.go
@@ -15,8 +15,7 @@ import (
 	"github.com/russross/blackfriday"
 )
 
-// TODO: create struct or map, return in loadEnv()
-var (
+type options struct {
 	port          string
 	assetsDir     string
 	markdownDir   string
@@ -24,10 +23,10 @@ var (
 	indexTemplate string
 	postTemplate  string
 	errorTemplate string
-)
+}
 
 func main() {
-	err := loadEnv()
+	options, err := loadEnv()
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -36,13 +35,13 @@ func main() {
 	r.Use(gin.Logger())
 	r.Delims("{{", "}}") // sets template tags
 
-	r.Use(static.Serve(assetsDir, static.LocalFile(assetsDir, false)))
-	r.LoadHTMLGlob("." + templateGlob)
+	r.Use(static.Serve(options.assetsDir, static.LocalFile(options.assetsDir, false)))
+	r.LoadHTMLGlob("." + options.templateGlob)
 
 	r.GET("/", func(c *gin.Context) {
 		var posts []string
 
-		files, err := ioutil.ReadDir("." + markdownDir)
+		files, err := ioutil.ReadDir("." + options.markdownDir)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -52,42 +51,45 @@ func main() {
 			posts = append(posts, file.Name())
 		}
 
-		c.HTML(http.StatusOK, indexTemplate, gin.H{"posts": posts})
+		c.HTML(http.StatusOK, options.indexTemplate, gin.H{"posts": posts})
 	})
 
 	r.GET("/:postTitle", func(c *gin.Context) {
 		postTitle := c.Param("postTitle")
 
-		mdFile, err := ioutil.ReadFile("." + markdownDir + "/" + postTitle)
+		mdFile, err := ioutil.ReadFile("." + options.markdownDir + "/" + postTitle)
 		if err != nil {
 			fmt.Println(err)
-			c.HTML(http.StatusNotFound, errorTemplate, nil)
+			c.HTML(http.StatusNotFound, options.errorTemplate, nil)
 			c.Abort()
 			return
 		}
 
 		postContent := template.HTML(blackfriday.MarkdownCommon([]byte(mdFile)))
 
-		c.HTML(http.StatusOK, postTemplate, gin.H{
+		c.HTML(http.StatusOK, options.postTemplate, gin.H{
 			"Title":   postTitle,
 			"Content": postContent,
 		})
 	})
 
-	r.Run(port) // TODO: look for interrupt
+	r.Run(options.port) // TODO: look for interrupt
 }
 
-func loadEnv() error {
-	err := godotenv.Load()
-	if err == nil {
-		port = os.Getenv("PORT")
-		assetsDir = os.Getenv("ASSETS_DIR")
-		markdownDir = os.Getenv("MARKDOWN_DIR")
-		templateGlob = os.Getenv("TEMPLATE_GLOB")
-		indexTemplate = os.Getenv("INDEX_TEMPLATE")
-		postTemplate = os.Getenv("POST_TEMPLATE")
-		errorTemplate = os.Getenv("ERROR_TEMPLATE")
+func loadEnv() (options, error) {
+	if err := godotenv.Load(); err != nil {
+		return options{}, err
 	}
 
-	return err
+	options := options{
+		port:          os.Getenv("PORT"),
+		assetsDir:     os.Getenv("ASSETS_DIR"),
+		markdownDir:   os.Getenv("MARKDOWN_DIR"),
+		templateGlob:  os.Getenv("TEMPLATE_GLOB"),
+		indexTemplate: os.Getenv("INDEX_TEMPLATE"),
+		postTemplate:  os.Getenv("POST_TEMPLATE"),
+		errorTemplate: os.Getenv("ERROR_TEMPLATE"),
+	}
+
+	return options, nil
 }
